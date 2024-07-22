134bc410f0c58f783d2d2c02149649d5f37629bd
Jhonatan RiBu <jhonatanribu25@gmail.com>
2024-07-22 12:26:08 -0500

    agregando urls project keymg

--- src/app/proyectos/project/project.component.ts
+++ src/app/proyectos/project/project.component.ts
@@ -18,8 +18,9 @@ export class ProjectComponent {
         'Key MG es un proyecto de innovación realizado en Python que busca canciones que combinen armónicamente con una canción seleccionada previamente. El fin del proyecto fue automatizar la busqueda creativa que tiene un DJ al momento de armar sus sets de canciones. ',
       imgUri: 'assets/images/keyMG.webp',
       tags: ['API Spotify', 'Python'],
-      projectUri: '',
-      githubUri: '',
+      projectUri:
+        'https://www.facebook.com/ACREDITACIONYCALIDADFIIS/videos/246305404871839',
+      githubUri: 'https://github.com/ribuhub25/keyMG',
     },
     {
       title: 'CANTOR HOME',
