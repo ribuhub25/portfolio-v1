0c191bcb4813366f69b495187ea9574a73172030
Jhonatan Riquelme <jhonatanribu25@gmail.com>
2025-08-18 11:38:32 -0500

    Actualización de navBar con Angular Material, responsive

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { MatIcon } from '@angular/material/icon';
-
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatIcon],
+  imports: [MatIconModule,MatToolbarModule, MatButtonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
