5db0a2b8c87f77189f0a1ad985042ef47ddda55b
Jhonatan Riquelme <jhonatanribu25@gmail.com>
2025-08-18 15:36:32 -0500

    Se hizo la configuración para la sección de presentación, incluye el diseño responsive

--- src/app/pages/home/section-presentation/section-presentation.component.ts
+++ src/app/pages/home/section-presentation/section-presentation.component.ts
@@ -10,5 +10,14 @@ import { MatCardModule } from '@angular/material/card';
   styleUrl: './section-presentation.component.scss'
 })
 export class SectionPresentationComponent {
-
+  years_experience : number = 3 
+  grade : string = "Egresado"
+  career : string = "Ingeniería de Sistemas"
+  university : string = "Universidad Nacional de Ingeniería"
+  city : string = "Lima"
+  country : string = "Perú"
+  speciality : string = "Desarrollo de Aplicaciones Web"
+  email : string = "jhonatanribu25@gmail.com"
+  linkedin : string = "https://linkedin.com/in/jhoribu25"
+  shortname : string = "Jhonatan Riquelme"
 }
