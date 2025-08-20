823fc2b9796b480b73736b47e5d8f1a3003be25e
Jhonatan Riquelme <jhonatanribu25@gmail.com>
2025-08-20 16:41:02 -0500

    correcciones para desplegar

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NotfoundComponent } from "../notfound/notfound.component";
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [NotfoundComponent],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
